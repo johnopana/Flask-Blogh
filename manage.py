36d6dc3be2df93d26762ac0ceb7c8a68bc1a1fcb
John <johnopana2016@gmail.com>
2020-01-17 23:36:39 +0300

    added unittest

--- manage.py
+++ manage.py
@@ -1,2 +1,44 @@
 from app import creat_app, db
 from flask_script import Manager, Server
+
+
+from app.models import Users,  post
+from flask_migrate import Migrate, MigrateCommand
+
+app = create_app('production')
+
+
+manager = Manager(app)
+
+
+migrate = Migrate(app, db)
+
+
+# Create manager instance 
+manager = Manager(app)
+
+# Create migrate instance
+migrate = Migrate(app, db)
+
+manager.add_command('server', Server)
+
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def test():
+    """
+    Run the unit tests
+    """
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Post=Post)
+
+
+if __name__ == '__main__':
+    manager.run()
