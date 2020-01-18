46479188f0244b5c3fcb5d6efe888df4012731b6
John <johnopana2016@gmail.com>
2020-01-18 10:35:35 +0300

    added tests

---
+++ tests/test_1.py
@@ -0,0 +1,22 @@
+import unittest
+# import to be tested
+# from app.models import[Classs]
+
+class Test(unittest.Testcase):
+
+    '''
+    Test class to test the behaviours of  the [class] class
+
+    Args:
+         unittest. TestCase : Test case  class that helps create test  cases
+
+         '''
+
+    def setUp(self):
+
+        '''
+        set up  method that will run every Test
+             
+        '''
+
+        pass
