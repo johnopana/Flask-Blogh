04f4844cd3db04877a14b1ba03d34be7afd41b52
John <johnopana2016@gmail.com>
2020-01-20 13:25:09 +0300

    added  comment.html

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,30 +1,42 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import Required, Email, EqualTo, Length
-from wtforms import ValidationError
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-
-from app.models import User
-
-
+from wtforms import StringField,PasswordField,SubmitField,ValidationError,BooleanField
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[Required(), Length(min=2, max=20)])
-    email = StringField('Your Email Address', validators=[Required(), Email()])
-    password = PasswordField('Password', validators={Required(),
-                                                     EqualTo('password_confirm', message='Passwords must match')})
-    password_confirm = PasswordField('Confirm Password', validators=[Required()])
-    submit = SubmitField('Sign Up')
-
-    def validate_email(self, data_field):
-        if User.query.filter_by(email=data_field.data).first():
-            raise ValidationError('There is an account with that email')
-
-    def validate_username(self, data_field):
-        if User.query.filter_by(username=data_field.data).first():
-            raise ValidationError('That username is taken')
-
-
+    """
+    RegstrationForm class that passes in the required details for validation
+    """
+    email = StringField('your email address',validators=[Required(),Email()])
+    username = StringField('your username',validators=[Required()])
+    password = PasswordField('password',validators=[Required(),EqualTo('password',message='passwords must match')])
+    password_confirm = PasswordField('confirm password',validators=[Required()])
+    submit = SubmitField('sign Up')
+    #custom validators
+    def validate_email(self,data_field):
+        '''
+        Functions takes in the data field and checks our database to confirm user Validation
+        '''
+        if User.query.filter_by(email = data_field.data).first():
+            raise ValidationError('there is an account with that email')
+    def validate_username(self,data_field):
+        '''
+        Function checks if the username is unique and raises ValidationError
+        '''
+        if User.query.filter_by(username = data_field.data).first():
+            raise ValidationError('that user name is already taken. Try another one')
+#login class  takes three inputs from the user
 class LoginForm(FlaskForm):
-    email = StringField('Your Email Address', validators=[Required(), Email()])
-    password = PasswordField('Password', validators=[Required()])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+    email = StringField('your email address',validators=[Required(),Email()])
+    password = PasswordField('password',validators=[Required()])
+    remember = BooleanField('remember me')
+    submit = SubmitField('sign in')
+
+
+
+
+
+
+
+
+
+
