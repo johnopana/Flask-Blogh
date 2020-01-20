a17c64e6eb175335bb1ba2664708fe672fc79a05
John <johnopana2016@gmail.com>
2020-01-20 17:44:51 +0300

    deployment to heroku

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 export SECRET_KEY='hghjslwdmslsokwpoiwhjwjwqoiwokq'
-# export MAIL_USERNAME='johnopana@gmail.com'
-# export MAIL_PASSWORD='johnopanawe'
-# export DATABASE_URL='postgres://luawbdsamsqazv:d5be903ed7c5bc3f4315c36e772f1e9f40b7d78f5b373f9838176ece76117772@ec2-54-83-59-144.compute-1.amazonaws.com:5432/db9o7g9nhms5nb'
-
-# export DATABASE_URL='postgres://moringa:john@localhost/pitchapp'
 python manage.py server
 
 
