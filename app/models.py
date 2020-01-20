e1f93cac79d1fd75165127c4622ab6af5eb3b356
John <johnopana2016@gmail.com>
2020-01-20 08:28:58 +0300

    created db

--- app/models.py
+++ app/models.py
@@ -1,13 +1,14 @@
 from datetime import datetime
-
-from flask import url_for, app, config
-from werkzeug.utils import redirect
-
+# from flask import url_for, app, config
+# from werkzeug.utils import redirect
 from . import db
 from flask_login import UserMixin
 from . import login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
